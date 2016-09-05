41bd7a1d2c72367305a1e8f510bc46bd2b14a398
Marten Hoekstra <marten@dolhuis.nl>
2016-09-05 14:44:53 +0200

    DevOps TG-175 added jenkins task to manage.py

--- web/authentication_service/authentication_service/settings.py
+++ web/authentication_service/authentication_service/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jenkins',
     'corsheaders',
     'authentication_service',
     'datapunt_api',
