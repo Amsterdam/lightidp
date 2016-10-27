1ca625ac6ae744dbbd364b102ec17ca9e0053b24
Jeroen van 't Hert <jeroen@yottabyte.io>
2016-10-27 09:26:32 +0200

    remove django_jenkins

--- web/authentication_service/authentication_service/settings.py
+++ web/authentication_service/authentication_service/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_jenkins',
     'corsheaders',
     'authentication_service',
     'datapunt_api',
