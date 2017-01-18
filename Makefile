5661a9a2bdf01a24e355197a28da929d5e3cc98f
Evert Lammerts <evert@lucipher.net>
2017-01-18 13:40:15 +0100

    tests

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 .PHONY: init test
 
+
 init:
-    pip install -r requirements.txt
+	pip install -r requirements.txt
 
 test:
-    py.test tests
+	# This runs all of the tests. To run an individual test, run py.test with
+	# the -k flag, like "py.test -k test_path_is_not_double_encoded"
+	py.test tests
+
+coverage:
+	py.test --cov-config .coveragerc --verbose --cov-report term --cov-report xml --cov=requests tests
+
--- tests/test_siam_client.py
+++ tests/test_siam_client.py
@@ -1,4 +1,5 @@
 import pytest
+import responses
 import auth.siam.client
 
 SIAM_BASE_URL = 'http://fake.siam'
@@ -9,10 +10,26 @@ SIAM_SHARED_SECRET = 'fake.secret'
 
 @pytest.fixture(scope="module")
 def siamclient():
-    client = siam.client.Client(
+    client = auth.siam.client.Client(
         base_url=SIAM_BASE_URL,
         app_id=SIAM_APP_ID,
         aselect_server=SIAM_ASELECT_SERVER,
         shared_secret=SIAM_SHARED_SECRET
     )
     return client
+
+
+@responses.activate
+def test_siam_server_error(siamclient):
+    responses.add(responses.GET, SIAM_BASE_URL, status=500)
+    try:
+        siamclient._request({}, 1.0)
+    except auth.siam.client.RequestException:
+        pass
+    else:
+        assert False, 'Client should have raised an exception'
+
+
+@responses.activate
+def test_get_authn_link(siamclient):
+    ...
