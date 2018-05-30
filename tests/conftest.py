8d79414acfcb7bb359a85974d46617bb20e6564a
Evert Lammerts <evert.lammerts@gmail.com>
2018-05-30 21:14:07 +0200

    no need for authz levels anymore

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,6 @@
 import os
 import pytest
 import auth.config
-import authorization
-
-
-@pytest.fixture(autouse=True)
-def no_database(monkeypatch):
-    authzmap = {
-        'employee': authorization.levels.LEVEL_EMPLOYEE,
-        'employeeplus': authorization.levels.LEVEL_EMPLOYEE_PLUS,
-    }
-
-    def AuthzMap(*args, **kwargs):
-        return authzmap
-    monkeypatch.setattr(authorization, 'AuthzMap', AuthzMap)
 
 
 @pytest.fixture(scope='session')
