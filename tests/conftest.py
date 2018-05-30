248b884f839425aaadd22e5c46e4a95a9c4b62fa
Evert Lammerts <evert.lammerts@gmail.com>
2018-05-30 21:20:29 +0200

    monkey patch dpuser

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import os
 import pytest
 import auth.config
+import dpuser
+
+@pytest.fixture(autouse=True)
+def no_database(monkeypatch):
+    def Users(*args, **kwargs):
+        return object()
+    monkeypatch.setattr(dpuser, 'Users', Users)
 
 
 @pytest.fixture(scope='session')
